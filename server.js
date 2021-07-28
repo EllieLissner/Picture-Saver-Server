97b452a2b51c9f2163534531d19bee843da88d15
Ellie-Lissner <eswalker0723@gmail.com>
2021-07-27 21:19:50 -0500

    added get route to render all images from database

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 
 let db = require('./models')
 const app = express()
 const PORT = process.env.PORT || 3001
 
 app.use(express.json())
+app.use(cors())
 
 //GET /user to view all of users saved images
-/*
+
 app.get('/', async (req, res) => {
-    ?? is this needed on server side? def needed on FE
-    need to send data to FE regarding the exact file location
-    so they can send the request to S3
+   try {
+       const images = await db.image.findAll()
+       const imagesWithLocation = images.filter((img)=> img.location)
+       res.send(imagesWithLocation)
+
+   }catch(error){
+       console.log(error)
+       res.status(500).send(error)
+   }
 })
-*/
+
 
 //POST '/' this is where, when the image is sent to S3, we 
 //save the author, location, and key, which are generated
@@ -22,7 +30,7 @@ app.get('/', async (req, res) => {
 //database
 app.post('/', async (req, res) => {
     try{
-        savedImage = await db.image.create({
+        await db.image.create({
             location: req.body.location,
             key: req.body.key,   
             user: req.body.author,
@@ -31,6 +39,7 @@ app.post('/', async (req, res) => {
         res.send('Image Saved')
     } catch (error) {
         console.log(error)
+        res.status(500).send(error)
     }
 })
 
